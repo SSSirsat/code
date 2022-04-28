fdb81082b0a837bb2dae42546c7a7e5e4ba41e4b
Shubham Sirsat <91066327+SSSirsat@users.noreply.github.com>
2022-04-28 07:45:08 +0530

    new file

---
+++ functions.py
@@ -0,0 +1,20 @@
+# important fuctions for maths
+
+class Operation:
+	def addition(a,b):
+		return a+b
+
+	def substraction(a,b):
+		if a>b:
+			c = a-b
+		else:
+			c = b-a
+		return c
+
+	def multipliction(a,b):
+		return a*b
+
+	def devision(a,b):
+		return a/b
+
+
